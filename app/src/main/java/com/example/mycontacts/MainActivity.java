ce274997189bc1db6b339ab7fe4369a67031f31b
Keerati Lertwiriyachai <keeratioat@gmail.com>
2020-11-01 10:34:12 +0700

    add หน้า user

--- app/src/main/java/com/example/mycontacts/MainActivity.java
+++ app/src/main/java/com/example/mycontacts/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.mycontacts;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,54 +23,63 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getName();
 
     private RecyclerView mRecyclerView;
+
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
+
         final AppExecutors executors = new AppExecutors();
         executors.diskIO().execute(new Runnable() {
             @Override
             public void run() {
                 AppDatabase db = AppDatabase.getInstance(MainActivity.this);
-                User[] users = db.userDao().getAllUsers();
-                UserAdapter adapter = new UserAdapter(MainActivity.this , users);
-                mRecyclerView.setAdapter(adapter);
-               /* String msg = "";
+                final User[] users = db.userDao().getAllUsers();
 
-                for(User u : users){
-                    Log.i(TAG , u.firstName + " " + u.lastName);
-                    msg += String.format(
-                            Locale.getDefault(),"%s %s %s\n",
-                            u.firstName ,u.lastName,DateFormatter.formatForUi(u.brithDate)
-                    );
-
-
-                }
-                final String message = msg;
                 executors.mainThread().execute(new Runnable() {
                     @Override
-                    public void run() { //main thread
-                       *//* new AlertDialog.Builder(MainActivity.this)
-                                .setMessage(message).
-                                setPositiveButton("OK",null)
-                                .show();*//*
+                    public void run() {
+                        UserAdapter adapter = new UserAdapter(MainActivity.this, users);
+                        mRecyclerView.setAdapter(adapter);
                     }
-                });*/
+                });
+
+        /*String msg = "";
+        for (User u : users) {
+          Log.i(TAG, u.firstName + " " + u.lastName);
+          msg += String.format(
+              Locale.getDefault(),
+              "%s %s %s\n",
+              u.firstName, u.lastName, DateFormatter.formatForUi(u.birthDate)
+          );
+        }
+
+        final String message = msg;
+        executors.mainThread().execute(new Runnable() {
+          @Override
+          public void run() { // main thread
+            new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", null)
+                .show();
+          }
+        });*/
             }
         });
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mRecyclerView = findViewById(R.id.user_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-
 
         Button addButton = findViewById(R.id.add_button);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this , AddUserActivity.class);
+                Intent i = new Intent(MainActivity.this, AddUserActivity.class);
                 startActivity(i);
             }
         });
